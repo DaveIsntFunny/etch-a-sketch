ac32b0056bf74f7b431800e492c005c71bb4ed2f
DaveIsntFunny <daveisntfunny@gmail.com>
2023-11-08 13:24:17 -0600

    fixes

--- script.js
+++ script.js
@@ -7,25 +7,34 @@ const button = document.querySelector("button");
 button.addEventListener("click",buttonPress);
 
 function drawCanvas(n){
+    const container = document.querySelector('div#container');
+    container.style.maxWidth = '960px';
+    container.display = 'flex';
     const canvas = document.querySelector('div#canvas');
-    canvas.maxWidth = n*20 +'px';
-    canvas.style.display = 'grid';
-    canvas.style.gridTemplate= n+' '+n;
+    //canvas.width = '80px';
+    // canvas.style.display = 'grid';
+    // canvas.style.gridTemplate= n+' '+n;
+    // canvas.style.columnGap= '0';
     canvas.innerHTML = '';
     //canvas.style.gridTemplateColumns = '20px 20px 20px 20px';
     for (let r = 0; r < n; r++){
-        // let row = document.createElement('div');
-        // row.style.display = 'grid';
-        // canvas.appendChild(row);
+         let row = document.createElement('div');
+         row.style.display = 'flex';
+         row.style.maxHeight = Math.min(20,960/n)+'px';
+         //row.style.minWidth = Math.min(600,960/n) + 'px';
+         canvas.appendChild(row);
         for (let c = 0; c < n; c++){
             let box = document.createElement('div');
             box.setAttribute('id',"r"+r+"c"+c);
-            box.style.height = '20px';
-            //box.style.maxWidth = '12px';
+            box.style.height = Math.min(20,960/n) + 'px';
+            box.style.width = Math.min(20,960/n) + 'px';
             box.style.border = '1px solid black';
+            box.style.padding = 0;
+            //box.style.display = 'inline';
+            box.style.margin = 0;
             //box.innerHTML = '<p> </p>';
-            box.style.gridArea = (r +1)+"/"+(c + 1);
-            canvas.appendChild(box);
+            //box.style.gridArea = (r +1)+"/"+(c + 1);
+            row.appendChild(box);
             box.addEventListener("mouseover", function() {
             this.style.backgroundColor = "black";
             }); 
@@ -33,4 +42,4 @@ function drawCanvas(n){
     }
 }
 
-drawCanvas(4);
+drawCanvas(16);
